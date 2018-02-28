b16895fd3aac109ff18dea7a786f8c1e5389bec1
timothy-tolley <timothytolley@outlook.com>
2018-03-01 11:19:53 +1300

    basic table

--- client/components/ExchangeDisplay.jsx
+++ client/components/ExchangeDisplay.jsx
@@ -1,1 +1,38 @@
 import React from 'react'
+
+
+class ExchangeDisplay extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+        <div className = 'exchangeContainer'>
+          <h1>This is the Exchange Container</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Buy</th>
+                <th>Sell</th>
+                <th>Diff %</th>
+                <th>Profitable</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    )
+  }
+
+}
+
+export default App
