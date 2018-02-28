0bfa937a202622f2827c03077dcad0c24ee3f237
Adam Kuhn <adam.kuhn20@gmail.com>
2018-03-01 11:08:40 +1300

    working dev script, nodemon and webpack

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
-import reducers from './reducers'
+// import reducers from './reducers'
 import App from './components/App'
 
 let store = createStore(reducers, compose(
