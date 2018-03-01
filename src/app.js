ac54a4303fd28456cc1cc423cde77c0fa4a7cdc8
timothy-tolley <timothytolley@outlook.com>
2018-03-01 18:16:59 +1300

    further clean up

--- src/app.js
+++ src/app.js
@@ -1,42 +1,30 @@
 const path = require('path')
 const favicon = require('serve-favicon')
-const compress = require('compression')
 
 const feathers = require('@feathersjs/feathers')
 const configuration = require('@feathersjs/configuration')
 const express = require('@feathersjs/express')
 const socketio = require('@feathersjs/socketio')
 
-const middleware = require('./middleware')
-const services = require('./services')
-
 const knex = require('./knex')
-
+const services = require('./services')
 const authentication = require('./authentication')
 
 const app = express(feathers())
 
-// Load app configuration
 app.configure(configuration())
-// Enable CORS, security, compression, favicon and body parsing
-app.use(compress())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
-// Host the public folder
 app.use('/', express.static(app.get('public')))
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
+
 
 // Set up Plugins and providers
-app.configure(socketio())
-
 app.configure(knex)
-
-// Configure other middleware (see `middleware/index.js`)
-app.configure(middleware)
-app.configure(authentication)
-// Set up our services (see `services/index.js`)
 app.configure(services)
-
+app.configure(socketio())
+app.configure(authentication)
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound())
