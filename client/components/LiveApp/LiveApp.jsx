db2301e5b3ce71188d06e242e218450848259f82
Adam Kuhn <adam.kuhn20@gmail.com>
2018-03-03 12:57:50 +1300

    removed test code in LiveApp

--- client/components/LiveApp/LiveApp.jsx
+++ client/components/LiveApp/LiveApp.jsx
@@ -9,28 +9,20 @@ import BestTrade from '../BestTrade/BestTrade.jsx'
 import DollarValues from '../DollarValues/DollarValues.jsx'
 import ExchangeDisplay from '../ExchangeDisplay/ExchangeDisplay.jsx'
 
-<<<<<<< HEAD
 import baseUrl from '../../lib/base-url'
 import {coinData} from '../../actions' 
 const {COIN_DATA} = require('../../../common/events')
 
 class LiveApp extends React.Component{
-=======
-class LiveApp extends React.Component {
->>>>>>> f90eb9eb6cf81108c7fdd623c7532894ffb04958
   constructor (props) {
     super(props)
     this.state = {
       coin_prices: {},
       numberOfRequests: 0,
       results: [],
-      socket: null,
-      data: false,
-      count: 0
     }
   }
 
-<<<<<<< HEAD
 componentDidMount () {
  const socket = io(baseUrl)
   socket.on(COIN_DATA, (data) => {
@@ -43,14 +35,6 @@ componentDidMount () {
   render(){
     return (
       <div className = 'liveApp'>
-      <h2>{this.state.count}</h2>
-      <h3>{this.state.data && this.state.data.name}</h3>
-=======
-  render () {
-    return (
-      <div className = 'liveApp'>
-        <h1> Delt&Delta;ge </h1>
->>>>>>> f90eb9eb6cf81108c7fdd623c7532894ffb04958
         <BestTrade/>
         <DollarValues />
         <ExchangeDisplay />
@@ -61,8 +45,4 @@ componentDidMount () {
   }
 }
 
-<<<<<<< HEAD
 export default connect()(LiveApp)
-=======
-export default LiveApp
->>>>>>> f90eb9eb6cf81108c7fdd623c7532894ffb04958
