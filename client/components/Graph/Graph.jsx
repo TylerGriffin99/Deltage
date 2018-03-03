1c12c2312f715c3ca37433719005aa703162584a
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2018-03-03 14:04:26 +1300

    got a responsive graph with data from API

--- client/components/Graph/Graph.jsx
+++ client/components/Graph/Graph.jsx
@@ -6,16 +6,12 @@ import axios from 'axios'
 import './graph.css'
 import graphData from './graphData'
 
-let labels=[]
-let data1=[]
-
 
 class Graph extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            labels: [],
-            data1: []
+            graphData
         }
     }
     componentDidMount(){
@@ -39,29 +35,26 @@ class Graph extends React.Component {
         .then(res => {
             let cryptos = res.data.RAW.BTC.USD
             let {LASTUPDATE, PRICE} = cryptos
-            labels.push(this.timeString(LASTUPDATE))
-            data1.push(PRICE)
-            if (labels.length > 20){
-                let removed=labels.shift()
-                removed=data1.shift()
-            }
+            const graphData = {...this.state.graphData}
+            graphData.labels = [...graphData.labels, this.timeString[LASTUPDATE]]
+            graphData.datasets[0].data = [...graphData.datasets[0].data, PRICE]
+            // if (graphData.labels.length > 20){
+            //     let removed=labels.shift()
+            //     removed=data1.shift()
+            // }
             // console.log(labels, data1)
-                this.setState({
-                labels: labels,
-                data1: data1
+            this.setState({
+                graphData
             })
         })
     }
     render() {
-        const data = graphData
-        data.labels = this.state.labels
-        data.datasets[0].data = this.state.data1
-        console.log(data)
+        console.log(this.state.graphData)
         // data.datasets[1].data = this.state.data2
         return (
             <div className="graph">
                 <Line
-                    data={data}
+                    data={this.state.graphData}
                     options={{}}
                 />
             </div>
--- client/components/Graph/graphData.js
+++ client/components/Graph/graphData.js
@@ -1,7 +1,7 @@
 import moment from 'moment'
 
 const graphData = {
-    labels: [],
+    labels: ["12:44:08 PM", "12:44:40 PM", "12:44:40 PM"],
     datasets: [
         {
             label: 'BitFoo',
@@ -22,29 +22,29 @@ const graphData = {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: []
+            data: [11014.99, 11017.12, 11017.12]
         },
-        {
-            label: 'BitBar',
-            fill: false,
-            lineTension: 0.0,
-            backgroundColor: 'rgba(75,72,192,0.4)',
-            borderColor: 'rgba(75,75,75,1)',
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: 'rgba(75,75,75,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,75,75,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: []
-        }
+        // {
+        //     label: 'BitBar',
+        //     fill: false,
+        //     lineTension: 0.0,
+        //     backgroundColor: 'rgba(75,72,192,0.4)',
+        //     borderColor: 'rgba(75,75,75,1)',
+        //     borderCapStyle: 'butt',
+        //     borderDash: [],
+        //     borderDashOffset: 0.0,
+        //     borderJoinStyle: 'miter',
+        //     pointBorderColor: 'rgba(75,75,75,1)',
+        //     pointBackgroundColor: '#fff',
+        //     pointBorderWidth: 1,
+        //     pointHoverRadius: 5,
+        //     pointHoverBackgroundColor: 'rgba(75,75,75,1)',
+        //     pointHoverBorderColor: 'rgba(220,220,220,1)',
+        //     pointHoverBorderWidth: 2,
+        //     pointRadius: 1,
+        //     pointHitRadius: 10,
+        //     data: [10014.99, 10017.12, 1017.12]
+        // }
     ]
 }
 
