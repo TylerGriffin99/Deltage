44ef6ddfe65d2264d79d2baa07941849af8f4db8
TylerGriffin99 <tylergriffin70@gmail.com>
2018-03-05 15:02:39 +1300

    start table filter

--- client/components/ExchangeDisplay/ExchangeDisplay.jsx
+++ client/components/ExchangeDisplay/ExchangeDisplay.jsx
@@ -4,9 +4,62 @@ import {connect} from 'react-redux'
 import './exchangeDisplay.css'
 
 class ExchangeDisplay extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      bittrex: true,
+      poloniex: true,
+      kraken: true,
+      checked: false
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange (e) {
+    // filter exchange results here
+
+
+    
+    this.setState({
+      checked: !this.state.checked
+    })
+  } 
+
   render () {
+    console.log(this.props.receivedData)
+    console.log(this.props.tableData)
     return (
       <div className = 'exchangeContainer'>
+        <div>
+          <form>
+        Select the exchanges you participate in
+            <label>
+          Bittrex:
+              <input
+                name="bittrex"
+                type="checkbox"
+                checked={this.state.checked}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+          Poloniex:
+              <input
+                name="poloniex"
+                type="checkbox"
+                checked={this.state.poloniexChecked}
+                onChange={this.handleInputChange} />
+            </label>
+            <label>
+          Kraken:
+              <input
+                name="kraken"
+                type="checkbox"
+                checked={this.state.krakenChecked}
+                onChange={this.handleInputChange} />
+            </label>
+          </form>
+        </div>
         <h1>Exchange Container</h1>
         <table className = 'exchangeTable'>
           <thead>
