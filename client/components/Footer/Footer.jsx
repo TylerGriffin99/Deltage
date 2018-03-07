8ae2819c19c6145ce48932cc70fa02736890c75b
timothy-tolley <timothytolley@outlook.com>
2018-03-07 17:48:51 +1300

    linting fixes

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 import './footer.css'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div className='footer'>
-                <p>
+  render () {
+    return (
+      <div className='footer'>
+        <p>
                     EDA Harakeke-2018 Project by
-                    </p>
-                <br />
-                <p>
+        </p>
+        <br />
+        <p>
                     Adam, Maddie, Peter, Tim &amp; Tyler
-                </p>
-            </div>
-        )
-    }
+        </p>
+      </div>
+    )
+  }
 }
 
 export default Footer
